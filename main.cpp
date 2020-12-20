ee9298fe8fb68f369f87df80c2fb0b0293a827a4
Ivan Rajkovic <iivanrajkovic@gmail.com>
2020-12-20 17:14:27 +0100

    Read paths from CLI:
    * Add tabs
    * Exit on signal

--- main.cpp
+++ main.cpp
@@ -8,9 +8,10 @@
 #include <thread>
 #include <functional>
 #include <future>
+#include <csignal>
 
-//#define LOG(line) std::cerr << line << std::endl;
-#define LOG(line) //
+#define LOG(line) std::cerr << __FUNCTION__ << ": " <<  line << std::endl;
+//#define LOG(line)
 
 class LogReader {
 
@@ -29,16 +30,14 @@ public:
 
     void start() {
         _worker = std::async(std::launch::async, [this] () {
-            while(read_line());
+            _isOn = true;
+            while(_isOn && read_line());
             _onStop();
         });
     }
 
-    std::string get_line(const size_t lineNo) {
-        std::lock_guard<std::mutex> g(_mtx);
-        if (lineNo < _lines.size()) {
-            return _lines[lineNo];
-        }
+    void stop() {
+        _isOn = false;
     }
 
     bool read_line() {
@@ -51,9 +50,8 @@ public:
         std::string line;
         std::getline(_file, line);
         if (!line.empty()) {
-            LOG("Read: \"" << line << "\", size: " << line.size());
+            // LOG("Read: \"" << line << "\", size: " << line.size());
             std::lock_guard<std::mutex> g(_mtx);
-            _lines.emplace_back(line); // TODO :: Remove internal buffer
             _onReadLine(line);
         }
 
@@ -67,13 +65,15 @@ private:
     OnStop                   _onStop;
     OnReadLine               _onReadLine;
     std::future<void>        _worker;
-    std::vector<std::string> _lines;
     std::mutex               _mtx;
+    std::atomic<bool>        _isOn;
 };
 
 class Curses {
 
+    static const size_t _menuHeight = 3u;
     bool _active = false;
+    std::vector<std::string> _tabs;
 
 public:
 
@@ -85,8 +85,11 @@ public:
 
         if (_active) {
             start_color();
-            init_pair(1, COLOR_RED, COLOR_YELLOW);
+            init_pair(1, COLOR_WHITE, COLOR_BLACK);
             init_pair(2, COLOR_GREEN, COLOR_BLACK);
+            init_pair(3, COLOR_YELLOW, COLOR_BLACK);
+            init_pair(4, COLOR_MAGENTA, COLOR_BLACK);
+            init_pair(5, COLOR_CYAN, COLOR_BLACK);
         }
 
         LOG("Curses: " << _active);
@@ -94,14 +97,35 @@ public:
         return _active;
     }
 
+    void addTab(std::string name, int index) {
+        LOG(_tabs.size() << " " << index);
+        if (_tabs.size() <= index) {
+            _tabs.resize(index + 1u);
+        }
+        _tabs[index] = name;
+    }
+
+    void drawMenu() {
+        size_t column = 2u;
+        for (int i=1; i<_tabs.size(); i++) {
+            attron(COLOR_PAIR(i));
+            mvprintw(1, column, "[%d] %s", i, _tabs[i].c_str());
+            column += _tabs[i].size() + 10u;
+        }
+    }
+
+    void redraw() {
+        drawMenu();
+        refresh();
+    }
+
     void print_line(int row, std::string line, int index) {
 
         if (_active) {
             attron(COLOR_PAIR(index));
-            mvprintw(row, 0, "%4d %s", row, line.c_str());
-            refresh();
+            mvprintw(row + _menuHeight, 0, "[%i] %s", index, line.c_str());
+            redraw();
             //LOG(row << ": " << line);
-
         }
     }
 
@@ -117,40 +141,77 @@ bool parse_options(int argc, char* argv[]) {
 
 class Screen {
 
-    size_t row = 0;
 public:
 
     Screen(Curses& curses) : _curses(curses) {}
 
-    void printLn(const std::string& line) {
-        ++row;
-        _curses.print_line(row, line, row % 2 + 1);
+    void addLine(const std::string& line, int index) {
+        std::lock_guard<std::mutex> g(_mtx);
+        ++_row;
+        _lines.emplace_back(line);
+        _curses.print_line(_row, line, index);
     }
 
+    void redraw() {
+        _curses.drawMenu();
+    }
+
+    std::function<void(std::string)> getAppender(std::string name, int index) {
+        LOG("Appender " << name << " (" << index << ")");
+        _curses.addTab(name, index);
+        return [this, index] (std::string line) {
+            addLine(line, index);
+        };
+    }
+
+private:
     Curses& _curses;
+    std::mutex  _mtx;
+    size_t      _row = 0;
+    std::vector<std::string> _lines;
 };
+
+namespace {
+    std::function<void(int)> shutdownHandler;
+    void signalHandler(int signal) { 
+        if (shutdownHandler) {
+            shutdownHandler(signal);
+        }
+    }
+}
 
 int main(int argc, char* argv[]) {
 
     Curses curses;
     curses.activate();
     Screen screen(curses);
+    std::mutex cvMtx;
+    std::condition_variable cv;
+    bool isRunning(true);
 
-    std::atomic<bool> isRunning(true);
-    const auto stop = [&isRunning](){ isRunning = false; LOG("Stopped."); };
-    const auto onReadLine = [&screen](auto line) { screen.printLn(line); };
+    const auto stop = [&cv, &cvMtx, &isRunning] () { 
+        { std::lock_guard<std::mutex> g(cvMtx); isRunning = false; } 
+        cv.notify_one(); 
+        LOG("Stopped."); 
+    };
+    const auto noop = [](){};
+
+    shutdownHandler = [&stop](int /*signal*/){ stop(); };
+    std::signal(SIGINT, signalHandler);
         
     std::vector<std::unique_ptr<LogReader>> readers;
-    readers.emplace_back(std::make_unique<LogReader>("in.txt", stop, onReadLine));
 
-    int row = 0;
-    std::string line;
+    for (int i=1; i<argc; i++) {
+        LOG("Reading file " << argv[i]);
+        readers.emplace_back(std::make_unique<LogReader>(argv[i], noop, screen.getAppender(argv[i], i)));
+    }
 
-    do {
-        curses.print_line(row, line, row % 2 + 1);
-        row++;
-        std::this_thread::sleep_for(std::chrono::seconds(5));
-    } while (isRunning);
+    std::unique_lock<std::mutex> g(cvMtx);
+    cv.wait(g, [&isRunning] () { return !isRunning;});
+
+    for (auto& reader : readers) {
+        reader->stop();
+    }
 
     return EXIT_SUCCESS;
 }
